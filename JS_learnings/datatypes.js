c8a70dd5ddbdd4d62d6bc35f664edb662deb92d3
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2024-08-24 12:36:33 +0000

    container config done

---
+++ JS_learnings/datatypes.js
@@ -0,0 +1,28 @@
+// Numbers:
+let length = 16;
+let weight = 7.5;
+console.log(typeof length)
+console.log(typeof weight)
+
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+console.log(typeof color)
+
+// Booleans
+let x = true;
+let y = false;
+console.log(typeof x)
+
+//null
+let a=null
+console.log(typeof a)
+
+//undefined
+let B
+console.log(typeof B)
+
+
+
+
