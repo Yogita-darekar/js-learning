aaa6c37f511e27c45113b6851f164210a95ebbd9
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2024-08-24 12:27:50 +0000

    container config done

--- JS_learnings/var_const.js
+++ JS_learnings/var_const.js
@@ -2,5 +2,5 @@ const accountId = 36649087
 let email ="hgsdla@.com"
 var password = "36579253"
 city="latur"
-console.log(accountId)
+console.log(accountId);
 console.table([accountId,password,email,city])
