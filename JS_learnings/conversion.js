a86ff04e6fb0cbb6de1c9ef05c12a39775b6476a
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2024-08-24 13:01:27 +0000

    container config done

---
+++ JS_learnings/conversion.js
@@ -0,0 +1,31 @@
+//converting string to number
+let a ="44"
+console.log(typeof a)
+let b = Number(a)
+console.log(b)
+console.log(typeof b)
+
+let m ="44dfhg"
+console.log(typeof m)
+let n = Number(m)
+console.log(n)           //NaN
+console.log(typeof n)
+
+//converting into boolean
+let x = 1
+console.log(typeof x)
+let y = Boolean(x)
+console.log(y)
+console.log(typeof y)
+
+let z = ""
+console.log(typeof z)
+let w = Boolean(z)
+console.log(w)
+console.log(typeof w)
+
+let p = "xyz"
+console.log(typeof p)
+let q = Boolean(p)
+console.log(q)
+console.log(typeof q)
