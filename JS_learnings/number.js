cd1b87f74fc08e4b6550cf46a2e8ba3e2c4695c2
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2024-08-29 14:53:44 +0000

    number done

---
+++ JS_learnings/number.js
@@ -0,0 +1,18 @@
+//number in js
+const score = 800
+console.log(score)
+ 
+const balance = new Number(200)
+console.log(balance)
+
+console.log(balance.toString().length)  //length of numbers
+
+console.log(balance.toFixed())         //fixed value
+
+console.log(balance.toPrecision(6))    //digit numbers after point
+
+console.log(balance.toLocaleString())
+
+let x = "20"
+let y = 30
+console.log(x+y)
