bc496043d8e6809c14a7e0fe809493934485cd66
Darekar Yogita <147601324+Yogita-darekar@users.noreply.github.com>
2024-08-29 14:39:43 +0000

    string functions done

---
+++ JS_learnings/string.js
@@ -0,0 +1,35 @@
+//string functions
+
+const name = new String('India is my country')
+let part = name.substr(5)
+console.log(name)
+console.log(part)          //substring function
+
+let slic = name.charAt(7)
+console.log(name)
+console.log(slic)           //character at that position
+
+let slie = name.slice(2,9)
+console.log(name)
+console.log(slie)            //slicing
+
+/*String toUpperCase()
+String toLowerCase()
+String concat()
+String trim()
+String trimStart()
+String trimEnd()
+String padStart()
+String padEnd()
+String repeat()
+String replace()
+String replaceAll()
+String split()
+String length
+String charAt()
+String charCodeAt()
+String at()
+String [ ]
+String slice()
+String substring()
+String substr()*/
